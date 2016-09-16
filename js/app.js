083a49868791587efd8fb78677b261dac757b543
alex.melagrano <alex.melagrano@mullenlowe.com>
2016-09-16 17:38:01 -0400

    fixing sass compiling issues, js start (?)

--- js/app.js
+++ js/app.js
@@ -7,3 +7,32 @@ var main = function() {
     initGame = new game();
     
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Initialization functions
+// ======================================
+
+// Creating the board
+var grid = [];
+
+var initBoard = function() {
+    canvas : document.createElement("canvas");
+    start : function() {
+        this.canvas.width = 500;
+        this.canvas.height = 500;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+    }
+    
+}
